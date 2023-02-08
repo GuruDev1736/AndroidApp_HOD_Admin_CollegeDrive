163254cc8b2032415f9036c41b2e4f5104f33619
guruprasad <guruprasad1736@gmail.com>
2023-02-09 01:30:51 +0530

    This is Commit

---
+++ app/src/main/java/com/example/attend/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.attend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.attend.Autenthication.Admin_login;
+import com.example.attend.Autenthication.admin_sign_up;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               Intent intent = new Intent(getApplicationContext(), Admin_login.class);
+               startActivity(intent);
+               finish();
+
+            }
+        },5000);
+
+    }
+
+
+}
